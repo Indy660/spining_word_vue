f26670b7333df6f43faf56de9f462cfb00a10ea2
Indy <indycar111@gmail.com>
2022-01-30 20:32:21 +0300

    Initial commit

---
+++ src/components/CellComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="cell"></div>
+</template>
+
+<script>
+export default {
+  name: "CellComponent",
+  props: {
+    index: {
+      type: Number,
+      default: 0,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.cell {
+  width: 100%;
+  height: 100%;
+  border: 1px solid #fff1;
+  z-index: 2;
+}
+</style>
---
+++ src/components/InscriptionName.vue
@@ -0,0 +1,35 @@
+<template>
+  <div  ref="name" class="name" :style="computedStyle">{{ inscription }}</div>
+</template>
+
+<script>
+export default {
+  name: "InscriptionName",
+  data() {
+    return {
+      inscription: 'CSS'
+    }
+  },
+  computed: {
+    computedStyle() {
+      return {
+        color: 'red',
+        fontSize: '13px'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.name {
+  font-family: 'Fredoka One', cursive;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  animation: color 3s infinite linear;
+  text-shadow: 0 0 10px #000a;
+  transition: all 0.5s;
+  color: wheat;
+}
+</style>
